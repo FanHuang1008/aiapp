867f74407c22b10b0808dfe6a07bbad88960e14e
daniel <bodomer247817@gmail.com>
2024-08-08 12:46:00 +0800

    CHANGE: read model name from config

--- aiapp/cli.py
+++ aiapp/cli.py
@@ -2,4 +2,5 @@ from . import __version__
 
 def cli():
     print("hello")
+    print("version:", __version__)
     from . import main
--- aiapp/main.py
+++ aiapp/main.py
@@ -1,9 +1,10 @@
 # pip install torch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 --index-url https://download.pytorch.org/whl/cpu
 from .model import load_model
 from .data import read_image
+from .configs import cfg
 
 # cli argument: $ aiapp ./test.png
-img = read_image("../test.png")
+img = read_image(cfg.infer_model_name)
 
 # configs: .env .ini .cfg <-- pydantic
 model = load_model('mobilenetv4_conv_small.e2400_r224_in1k', pretrained=True)
